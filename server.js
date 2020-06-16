354342e3a8c3a34053d6dfefb17d1097868c2109
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-06-15 17:29:57 -0700

    update css

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
@@ -34,7 +34,7 @@ app.get('/searches/show', (request, response) => {
 app.post('/searches', (request, response) => {
     let query = request.body.search[0]
     let titleorAuthor = request.body.search[1]
-    console.log(request.body);
+    // console.log(request.body);
 
     let url = `https://www.googleapis.com/books/v1/volumes?q=+in${titleorAuthor}:${query}`;
 
@@ -47,32 +47,33 @@ app.post('/searches', (request, response) => {
     superagent.get(url)
         .then(results => {
             let bookArray = results.body.items;
-            console.log(results.body.items);
+            console.log(results.body.items[0].volumeInfo.imageLinks);
+
             let totalBookArray = bookArray.map(book => {
                 return new Book(book.volumeInfo)
             });
-            response.render('pages/searches/show.ejs', {searchResults: totalBookArray})
+            response.render('pages/searches/show.ejs', { searchResults: totalBookArray })
         }).catch(error => errorHandler(error, request, response))
 })
 
 function Book(obj) {
     this.title = obj.title || 'Title not available'
     this.authors = obj.authors || 'Author not available'
-    this.image_url = obj.image_url || 'https://i.imgur.com/J5LVHEL.jpg'
+    this.image_url = obj.imageLinks.thumbnail ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
     this.description = obj.description || 'Description not available'
 }
 
 function errorHandler(error, request, response) {
-    response.status(500).send({status: 500, responseText: 'That did not go as expected'});
-  }
+    response.status(500).send({ status: 500, responseText: 'That did not go as expected' });
+}
 
-app.get('*',(request, response) => {
+app.get('*', (request, response) => {
     response.status(404).send('Sorry, that did not work');
 })
 
 // dbClient.connect()
 //     .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on ${PORT}`);
-        })
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+})
     // });
