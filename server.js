b76a7500d407ef96dfcfbd125eccdac40c952ddf
racarter1215 <racarter1215@gmail.com>
2020-06-16 15:45:52 -0700

    lab12 card 1 done

--- server.js
+++ server.js
@@ -4,8 +4,9 @@ const express = require('express');
 require('ejs');
 require('dotenv').config();
 const superagent = require('superagent');
-// const pg = require('pg');
-// const dbClient = new pg.Client(process.env.DATABASE_URL);
+const pg = require('pg');
+const { response } = require('express');
+const dbClient = new pg.Client(process.env.DATABASE_URL);
 const app = express();
 
 const PORT = process.env.PORT || 3001;
@@ -15,9 +16,15 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
-app.get('/', (request, response) => {
-    response.render('pages/index.ejs');
-})
+app.get('/', renderIndex);
+
+function renderIndex(request, response) {
+    let sql = 'SELECT * FROM books;';
+    dbClient.query(sql)
+        .then(databaseSearchResults => {
+            response.render('pages/index.ejs', { homeArray: databaseSearchResults.rows })
+        }).catch(error => errorHandler(error, request, response))
+}
 
 app.get('/hello', (request, response) => {
     response.render('pages/index.ejs');
@@ -38,17 +45,11 @@ app.post('/searches', (request, response) => {
 
     let url = `https://www.googleapis.com/books/v1/volumes?q=+in${titleorAuthor}:${query}`;
 
-    // if(titleorAuthor === 'title') {
-    //     url+=`+intitle:${query}`;
-    // }else if(titleorAuthor === 'author'){
-    //     url=+`inauthor:${query}`;
-    // }
-
     superagent.get(url)
         .then(results => {
             let bookArray = results.body.items;
-            console.log(results.body.items[0].volumeInfo.imageLinks);
-
+            // console.log(results.body.items[0].volumeInfo.bookshelf);
+           
             let totalBookArray = bookArray.map(book => {
                 return new Book(book.volumeInfo)
             });
@@ -56,11 +57,26 @@ app.post('/searches', (request, response) => {
         }).catch(error => errorHandler(error, request, response))
 })
 
+function addToDatabase() {
+    let {title, authors, image_url, description, isbn, bookshelf} = results.body
+    let sqlAdd = `INSERT INTO books (title, authors, image_url, description, isbn, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING ID;`;
+    let safeValues = [book.volumeInfo.title, book.volumeInfo.authors, book.volumeInfo.image_url, book.volumeInfo.description, book.volumeInfo.isbn, book.volumeInfo.bookshelf]
+    dbClient.query(sqlAdd, safeValues)
+        .then(store => {
+            let id = store.rows[0].id;
+            response.redirect(`/${id}`)
+                
+                
+            })
+}
+
 function Book(obj) {
     this.title = obj.title || 'Title not available'
     this.authors = obj.authors || 'Author not available'
     this.image_url = obj.imageLinks.thumbnail ? obj.imageLinks.thumbnail : 'https://i.imgur.com/J5LVHEL.jpg';
     this.description = obj.description || 'Description not available'
+    this.isbn = obj.industryIdentifiers[0].identifier;
+    // this.bookshelf = obj.
 }
 
 function errorHandler(error, request, response) {
@@ -71,9 +87,9 @@ app.get('*', (request, response) => {
     response.status(404).send('Sorry, that did not work');
 })
 
-// dbClient.connect()
-//     .then(() => {
+dbClient.connect()
+    .then(() => {
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`);
 })
-    // });
+    });
