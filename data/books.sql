57b9abc1df0671fd9a2065f85eeef2d6dc2fe865
racarter1215 <racarter1215@gmail.com>
2020-06-16 18:31:07 -0700

    trello boards 2 and 3 done

--- data/books.sql
+++ data/books.sql
@@ -2,16 +2,16 @@ DROP TABLE IF EXISTS books;
 
 CREATE TABLE books (
     id SERIAL PRIMARY KEY,
-    author VARCHAR(255),
+    authors VARCHAR(255),
     title VARCHAR(255),
-    isbn INTEGER,
+    isbn NUMERIC,
     image_url VARCHAR(255),
-    description VARCHAR(255),
+    description TEXT,
     bookshelf VARCHAR(255)
 );
 
 INSERT INTO books (
-    author,
+    authors,
     title,
     isbn,
     image_url,
