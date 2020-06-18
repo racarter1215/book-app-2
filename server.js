8e004e5c19ccec32f2452b954a278cca8da4314a
Joshua Williams <joshuawilliamsguitar@gmail.com>
2020-06-17 22:50:39 -0700

    added dropdown to homepage

--- server.js
+++ server.js
@@ -17,18 +17,30 @@ app.set('view engine', 'ejs');
 app.use(methodOverride('_method'));
 
 app.get('/', renderHomePage);
+app.put('/bookshelf', displayBookshelf);
 app.get('/searches/new', searchForm);
 app.post('/searches', getBooksFromAPI);
 app.post('/books', addToDatabase);
 app.get('/books/:id', bookRequest);
 app.put('/update/:books_id', updateBooks);
-// app.delete(, deleteBook);
+app.delete( '/delete/:id', deleteBook);
 app.get('*', (request, response) => {
     response.status(404).send('Sorry, that did not work');
 })
 
+
+function displayBookshelf (request,response) {
+    const bookshelf = request.body.bookshelf;
+    let sql = 'SELECT DISTINCT id, title, authors, image_url, description, isbn, bookshelf FROM books WHERE bookshelf=$1;';
+    let safeValue = [bookshelf];
+    dbClient.query(sql, safeValue)
+        .then(databaseSearchResults => {
+            response.render('pages/index.ejs', { homeArray: databaseSearchResults.rows })
+        }).catch(error => errorHandler(error, request, response))
+}
+
 function renderHomePage(request, response) {
-    let sql = 'SELECT DISTINCT title, authors, image_url, description, isbn, bookshelf FROM books;';
+    let sql = 'SELECT DISTINCT id, title, authors, image_url, description, isbn, bookshelf FROM books;';
     dbClient.query(sql)
         .then(databaseSearchResults => {
             response.render('pages/index.ejs', { homeArray: databaseSearchResults.rows })
@@ -48,7 +60,7 @@ function getBooksFromAPI(request, response) {
         .then(results => {
             let bookArray = results.body.items;
             let totalBookArray = bookArray.map(book => {
-                return new Book(book.volumeInfo)
+                return new Book(book.volumeInfo);
             });
             response.render('pages/searches/show.ejs', { searchResults: totalBookArray })
         }).catch(error => errorHandler(error, request, response))
@@ -58,35 +70,81 @@ function searchForm(request, response) {
     response.render('pages/searches/new.ejs');
 }
 
+function deleteBook(request,response) {
+    let deleteId = request.params.id;
+    let sql = 'DELETE FROM books WHERE id = $1;';
+    let safeValue = [deleteId];
+    dbClient.query(sql, safeValue)
+        .then(()=>
+        response.status(200).redirect('/'))
+        .catch(error => errorHandler(error, request, response));
+}
+
 function addToDatabase(request, response) {
-    let {title, authors, image_url, description, isbn} = request.body
-    let sqlAdd = 'INSERT INTO books (title, authors, image_url, description, isbn) VALUES ($1, $2, $3, $4, $5) RETURNING id;';
-    let safeValues = [title, authors, image_url, description, isbn]
+    let sql = 'SELECT * FROM books WHERE isbn = $1;';
+    let safeValue = [request.body.isbn];
+    dbClient.query(sql,safeValue)
+        .then(result => {
+        if (!result.rowCount) {
+    let { title, authors, image_url, description, isbn, bookshelf } = request.body;
+    let sqlAdd = 'INSERT INTO books (title, authors, image_url, description, isbn, bookshelf) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;';
+    let safeValues = [title, authors, image_url, description, isbn, bookshelf];
     dbClient.query(sqlAdd, safeValues)
         .then(store => {
             let id = store.rows[0].id;
-            console.log(id);
-            response.status(200).redirect(`/books/${id}`) 
+            response.status(200).redirect(`/books/${id}`)
         }).catch(error => console.log(error))
+        } else {
+            response.status(200).redirect(`/books/${result.rows[0].id}`);
+        }
+    })
 }
 
+
 function bookRequest(request, response) {
+    // let bookshelf = getBookShelfs();
+    // console.log('bookshelf array', bookshelf);
     let id = request.params.id;
     let sql = 'SELECT * FROM books WHERE id=$1;';
     let safeValues = [id];
-
     dbClient.query(sql, safeValues)
         .then(display => {
             response.status(200).render('./pages/books/show.ejs', {homeArray: display.rows});
         }).catch(error => errorHandler(error, request, response))
 }
 
+// function bookRequest(request, response) {
+//     let rows = getBookShelfs();
+//     let display = getHomeArray(request);
+//     response.status(200).render('./pages/books/show.ejs', { homeArray: display, bookshelfArray: rows});
+// }
+
+// function getBookShelfs () {
+//   let bsSql = 'SELECT DISTINCT bookshelf FROM books;';
+//   dbClient.query(bsSql)
+//     .then(results => {
+//       console.log('sql results', results.rows);
+//       return results.rows;
+//     }).catch(error => console.log(error));
+// }
+// function getHomeArray(request) {
+//     let id = request.params.id;
+//     let sql = 'SELECT * FROM books WHERE id=$1;';
+//     let safeValues = [id];
+//     dbClient.query(sql, safeValues)
+//         .then(display => {
+//           return display.rows;
+//         }).catch(error => errorHandler(error, request, response));
+// }
+
+
+
+
 function updateBooks(request, response) {
-    let {title, authors, description, image_url, isbn, bookshelf} = request.body;
+    let { title, authors, description, image_url, isbn, bookshelf } = request.body;
     let sql = 'UPDATE books SET authors=$1, title=$2, isbn=$3, image_url=$4, description=$5, bookshelf=$6 WHERE id=$7;';
     let booksId = request.params.books_id;
     let safeValues = [authors, title, isbn, image_url, description, bookshelf, booksId];
-    console.log(request.body);
     dbClient.query(sql, safeValues)
         .then(sqlResults => {
             console.log(sqlResults.rows)
@@ -109,7 +167,7 @@ function errorHandler(error, request, response) {
 
 dbClient.connect()
     .then(() => {
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-})
+        app.listen(PORT, () => {
+            console.log(`Server is running on ${PORT}`);
+        })
     });
